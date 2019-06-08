702df71946c7194bbb6b22f08048939458a4cc11
oviya <oviyaraja2626@gmail.com>
2019-06-07 17:45:44 -0700

    Code for Count Occurence added

---
+++ src/Occurence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Occurence
+{
+
+        public static void main(String args[])
+        {
+            Scanner s=new Scanner(System.in);
+            System.out.println("enter a string");
+            String str=s.nextLine();
+            int c=0;
+            char[] strArray=str.toCharArray();
+            for(int i=0;i<strArray.length;i++)
+            {
+                for(int j=0;j<strArray.length;j++)
+                {
+                    if(strArray[i]==strArray[j])
+                    {
+                        c++;
+                    }
+                }
+                System.out.print(strArray[i]);
+                System.out.print(c);
+                c=0;
+            }
+        }
+    }
+
